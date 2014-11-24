cc01cbc409df63adb23c5df344ef6490dc8f01d9
Liz Cole <liz.cole1227@gmail.com>
2014-11-24 16:26:04 -0600

    changed space settings

--- public/index_address_book.php
+++ public/index_address_book.php
@@ -9,7 +9,7 @@ $addresses = $dbc->query("SELECT * FROM people LEFT JOIN address ON address.peop
 
 <html>
 <head>
-	<title>Address Book</title>
+    <title>Address Book</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
@@ -21,58 +21,58 @@ $addresses = $dbc->query("SELECT * FROM people LEFT JOIN address ON address.peop
 </head>
 <body class='container'>
 
-	<h1>Address Book</h1>
+    <h1>Address Book</h1>
 
-	<table class='table table-bordered'>
-		<? foreach($addresses as $address):?>
-			<tr>
-				<th>Person</th>
-				<th>Address</th>
-			</tr>
-			<tr>
-				<td>
-					<?= $address['first_name'] ?>
-				</td>
-				<td>
-					<?= $address['last_name'] ?>
-				</td>
-				<td>
-					<?= $address['phone'] ?>
-				</td>
-				<td>
-					<?= $address['Street'] ?>
-				</td>
-				<td>
-					<?= $address['City'] ?>
-				</td>
-				<td>
-					<?= $address['State'] ?>
-				</td>
-				<td>
-					<?= $address['Zip'] ?>
-				</td>
-			</tr>
-		<? endforeach ?>
-	</table>
+    <table class='table table-bordered'>
+        <? foreach($addresses as $address):?>
+            <tr>
+                <th>Person</th>
+                <th>Address</th>
+            </tr>
+            <tr>
+                <td>
+                    <?= $address['first_name'] ?>
+                </td>
+                <td>
+                    <?= $address['last_name'] ?>
+                </td>
+                <td>
+                    <?= $address['phone'] ?>
+                </td>
+                <td>
+                    <?= $address['Street'] ?>
+                </td>
+                <td>
+                    <?= $address['City'] ?>
+                </td>
+                <td>
+                    <?= $address['State'] ?>
+                </td>
+                <td>
+                    <?= $address['Zip'] ?>
+                </td>
+            </tr>
+        <? endforeach ?>
+    </table>
 
-	<div class='row'>
-		<form role="form">
-			<div class="form-group col-md-3">
-				<label for='first_name'>First Name</label>
-				<input type='text' id='first_name' class="form-control">
-			</div>
-		
-			<div class="form-group col-md-3">
-				<label for='last_name'>Last Name</label>
-				<input type='text' id='last_name' class="form-control">
-			</div>
+    <div class='row'>
+        <form role="form">
+            <div class="form-group col-md-3">
+                <label for='first_name'>First Name</label>
+                <input type='text' id='first_name' class="form-control">
+            </div>
+        
+            <div class="form-group col-md-3">
+                <label for='last_name'>Last Name</label>
+                <input type='text' id='last_name' class="form-control">
+            </div>
 
-			<div class="form-group col-md-3">
-				<label for='phone_number'>Phone Number</label>
-				<input type='text' id='phone_number' class="form-control">
-			</div>
-			<button id='addperson' type='submit' class="btn btn-success">Add</button>
-		</form>
-	</div>
+            <div class="form-group col-md-3">
+                <label for='phone_number'>Phone Number</label>
+                <input type='text' id='phone_number' class="form-control">
+            </div>
+            <button id='addperson' type='submit' class="btn btn-success">Add</button>
+        </form>
+    </div>
 </body>
 </html>
