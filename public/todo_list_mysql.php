4245471b008fc044d27fe61b01b5caafe8a51bbc
Liz Cole <liz.cole1227@gmail.com>
2014-11-24 15:07:44 -0600

    added files for finilization of v. 1.0 of todo-list

--- public/todo_list_mysql.php
+++ public/todo_list_mysql.php
@@ -26,18 +26,21 @@ if(isset($_GET['id'])){
 
 }
 
-if(!empty($_POST['new_item'])) {
+if(!empty($_POST)) {
 
-	$newtodo = $dbc->prepare("INSERT INTO items(content) VALUES (:content)");
+	if(!empty($_POST['new_item'])) {
 
-	$newtodo->bindValue(':content', $_POST['new_item'], PDO::PARAM_STR);
+		$newtodo = $dbc->prepare("INSERT INTO items(content) VALUES (:content)");
 
-	$newtodo->execute();
+		$newtodo->bindValue(':content', $_POST['new_item'], PDO::PARAM_STR);
 
-} else if(empty($_POST['new_item'])){
+		$newtodo->execute();
 
-	$error = 'Please Enter All Fields';
-}
+	} else if(empty($_POST['new_item'])){
+
+		$error = 'Please Enter An Item';
+	}
+}	
 
 $object = $dbc->prepare("SELECT id, content FROM items LIMIT 10 OFFSET :offset_num");
 
@@ -63,6 +66,7 @@ $todo_list = $object->fetchAll(PDO::FETCH_ASSOC);
 	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Old+Standard+TT:400,400italic,700|Special+Elite' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
 	
 	<!-- CSS Sheet -->
 	<link rel="stylesheet" type="text/css" href="css/stylesheet_mysql.css">
@@ -72,7 +76,13 @@ $todo_list = $object->fetchAll(PDO::FETCH_ASSOC);
 	<div class='container'>
 		<body>
 
-			<h1 class="header jumbotron">ToDo List</h1>
+			<h1>ToDo List</h1>
+
+			<?if(isset($error)):?>
+				<div class="alert alert-danger" role="alert">
+					<?= $error; ?>
+				</div>
+			<? endif; ?>
 
 			<!-- this form allows for new items to be added to the todo list -->
 			<div class="newitem">
@@ -85,29 +95,38 @@ $todo_list = $object->fetchAll(PDO::FETCH_ASSOC);
 				</form>
 			</div>
 
-			<div id="list">
-				<ul id="scribble">
+				<table id="scribble">
 					<!-- creating a foreach loop in php within the html so php is actually in control
 					of the list items being added -->			
 					<? foreach($todo_list as $value): ?>
-					<li>
-						<?= htmlspecialchars(strip_tags($value['content'])); ?> | <a href="?id=<?= $value['id'] ?>">Complete</a>
-					</li>
+						<tr>
+							<td>
+								<?= htmlspecialchars(strip_tags($value['content'])); ?>  
+							</td>
+							<td>
+								<a href="?id=<?= $value['id'] ?>"><span class='glyphicon glyphicon-ok'><span></a>
+							</td>
+						</tr>
 					<? endforeach ?>
-				</ul>
-			</div>
+				</table>
 
-			<a href="?page=<?= $page_num - 1 ?>">
-				<button type="button" class="btn btn-default btn-sm">
-					Previous
-				</button>
-			</a>
+			<img id='yellowpaper' src="images/Yellow_notebook_paper.png">
 
+			<? if($page_num >= 2): ?>
+				<a href="?page=<?= $page_num - 1 ?>">
+					<button type="button" class="btn btn-default btn-sm">
+						Previous
+					</button>
+				</a>
+			<? endif; ?>	
+
+			<? if($page_num <= $count/10):?>
 			<a href="?page=<?= $page_num + 1 ?>">
 				<button type="button" class="btn btn-default btn-sm">
 					Next
 				</button>
 			</a>	
+		<? endif; ?>
 			
 		<!-- JS script cdn and link to js sheet -->
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
